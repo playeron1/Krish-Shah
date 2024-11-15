c2e6a12c6fafb6f1d82750678aa8d4888632f6c1
Krish Shah <krishnshah2003@gmail.com>
2024-11-15 18:21:27 +0530

    ..

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'selector', // Enable class-based dark mode
+  content: ['./**/*.html'], // Ensure Tailwind scans your HTML files
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};

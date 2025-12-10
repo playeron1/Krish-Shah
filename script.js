1c3c9b84c4f186c6a80e33a71249bddeb000bca9
playeron1 <krishnshah2003@gmail.com>
2025-12-10 19:18:40 +0530

    Updated UI for certifications section and fixed mobile menu z-index issue.

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-const anim1 = document.getElementById
+let menuList = document.getElementById("menuList");
+      menuList.style.maxHeight = "0px";
+      function toggleMenu() {
+        if (menuList.style.maxHeight == "0px") {
+          menuList.style.maxHeight = "500px";
+        }
+        else {
+          menuList.style.maxHeight = "0px";
+        }
+      }
+      GitHubCalendar(".calendar", "playeron1", { responsive: true });
